a4f77e56b5398dd4bd43ddf84d9e6327e91c35cf
julmasmurffi <js.kaikkonen@gmail.com>
2019-03-28 00:16:06 +0200

    Enemies can now take damage

---
+++ Space_Bullet_Hell/Assets/Scripts/DamageDealer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//a general class to rule over all damage in the game
+public class DamageDealer : MonoBehaviour
+{
+    //conf params
+    [SerializeField] int damage = 1;
+
+    //methods
+    public int GetDamage() { return damage; }
+
+    public void Hit() { Destroy(gameObject); }
+
+
+}
---
+++ Space_Bullet_Hell/Assets/Scripts/Enemy.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] float health = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+  
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
+        HpCheck(damageDealer);
+    }
+
+    private void HpCheck(DamageDealer damageDealer)
+    {
+        health -= damageDealer.GetDamage();
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
