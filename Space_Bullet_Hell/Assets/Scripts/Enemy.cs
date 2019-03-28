8b04b51c61bc0f9e67764b0074c05067baf63325
julmasmurffi <js.kaikkonen@gmail.com>
2019-03-28 17:53:25 +0200

    Enemy firing method preparations

--- Space_Bullet_Hell/Assets/Scripts/Enemy.cs
+++ Space_Bullet_Hell/Assets/Scripts/Enemy.cs
@@ -4,19 +4,41 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] GameObject LaserPrefab;
+    [SerializeField] float projectileSpeed = -10f;
+
     [SerializeField] float health = 1;
+    [SerializeField] float shotCounter;
+    [SerializeField] float minTimeBetweenShots = 0.2f;
+    [SerializeField] float maxTimeBetweenShots = 3f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
     }
 
     // Update is called once per frame
     void Update()
     {
-  
+        CountDownAndShoot();
         
+    }
+
+    private void CountDownAndShoot()
+    {
+        //when zero shoot
+        shotCounter -= Time.deltaTime;
+        if(shotCounter <= 0f)
+        {
+            Fire();
+        }
+    }
+
+    private void Fire()
+    {
+        GameObject laser = Instantiate(LaserPrefab, transform.position, Quaternion.identity) as GameObject;
+        laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, projectileSpeed);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
